d6c94b1e9352c62e9e8512d6712538f312a6467d
Anahita <anahita.as78@gmail.com>
2020-11-06 15:01:31 +0330

    changed data

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,24 +3,26 @@
       <home-flow></home-flow>       
       <app-recently-played :recentlyPlayed="recentlyPlayed"></app-recently-played>
       <app-categories></app-categories>
-      <app-songs :title='productive'
-                 :names='productiveName'
+      <app-songs :title='productive.title'
+                 :names='productive.name'
                  :viewAll="false"
                  :playlist="true"
                  :header="true"
-                 :urls='productiveUrl'></app-songs>
-      <app-songs :title='playlists'
-                 :names='playlistName'
+                 :urls='productive.url'
+                 :detail="productive.detail"></app-songs>
+      <app-songs :title='playlists.title'
+                 :names='playlists.name'
                  :viewAll="false"
                  :playlist="true"
                  :header="true"
-                 :urls='playlistUrl'></app-songs>
-      <app-songs :title='realeased'
-                 :names='songsName'
-                 :songNames='songNames'
+                 :urls='playlists.url'
+                 :detail="playlists.detail"></app-songs>
+      <app-songs :title='songs.title'
+                 :names='songs.name'
                  :viewAll="true"
                  :header="true"
-                 :urls='songsUrl'></app-songs>
+                 :urls='songs.url'
+                 :detail="songs.detail"></app-songs>
       <app-artists :header="true"
                    :artisturl="artisturl"></app-artists>
     </div>
@@ -94,65 +96,88 @@ export default {
           detail: ['sdsax']
         }
       ],
-      // productive: [
-      //   title: 
-      // ],
-      productiveUrl: [
-        require("../assets/Images/Deep Focus.png"),
-        require("../assets/Images/White Noise.png"),
-        require("../assets/Images/Nature.png"),
-        require("../assets/Images/Productive Morning.png"),
-        require("../assets/Images/Brain Food.png"),
-        require("../assets/Images/Morning Rush.png")
-      ],
-      productiveName:[
-        'Deep Focus',
-        'Productive Morning',
-        'White Noise',
-        'Nature Song',
-        "Brain Food",
-        'Morning Rush'
-      ],
-      playlistUrl: [
-        require("../assets/Images/80s alternative rock.png"),
-        require("../assets/Images/GUITAR SOLOS.png"),
-        require("../assets/Images/ROCK ballads.png"),
-        require("../assets/Images/ROCKABILLY.png"),
-        require("../assets/Images/WORKOUT rock.png"),
-        require("../assets/Images/COMPOSERS.png")
-      ],
-      playlistName:[
-        'Russian Composers',
-        'Guitar Solos',
-        'Workout Rock',
-        'Rock Ballads',
-        "80s Alternative",
-        'Rockabilly'
-      ],
-      songsUrl: [
-        require("../assets/Images/Image 418.png"),
-        require("../assets/Images/Image 419.png"),
-        require("../assets/Images/Image 693.png"),
-        require("../assets/Images/Image 695.png"),
-        require("../assets/Images/Image 696.png"),
-        require("../assets/Images/Mask Group 237.png")
-      ],
-      songsName:[
-        'LPS',
-        'Numb Numb Juice',
-        'Drip or Down 2',
-        'Shadows',
-        "Don't Feed the Pop Monster",
-        'HEF'
-      ],
-      songNames: [
-        'aaaa',
-        'aaaa',
-        'aaaa',
-        'aaaa',
-        'aaaa',
-        'aaaa'
-      ],
+      productive: {
+        title: "Make monday more producitve",
+        url: [
+          require("../assets/Images/Deep Focus.png"),
+          require("../assets/Images/White Noise.png"),
+          require("../assets/Images/Nature.png"),
+          require("../assets/Images/Productive Morning.png"),
+          require("../assets/Images/Brain Food.png"),
+          require("../assets/Images/Morning Rush.png")
+        ],
+        name: [
+          'Deep Focus',
+          'Productive Morning',
+          'White Noise',
+          'Nature Song',
+          "Brain Food",
+          'Morning Rush'
+        ],
+        detail: [
+          "244,412",
+          "244,412",
+          "244,412",
+          "244,412",
+          "244,412",
+          "244,412"
+        ]
+      },
+      playlists: {
+        title: "Playlists picks",
+        url: [
+          require("../assets/Images/80s alternative rock.png"),
+          require("../assets/Images/GUITAR SOLOS.png"),
+          require("../assets/Images/ROCK ballads.png"),
+          require("../assets/Images/ROCKABILLY.png"),
+          require("../assets/Images/WORKOUT rock.png"),
+          require("../assets/Images/COMPOSERS.png")
+        ],
+        name: [
+          'Russian Composers',
+          'Guitar Solos',
+          'Workout Rock',
+          'Rock Ballads',
+          "80s Alternative",
+          'Rockabilly'
+        ],
+        detail: [
+          "71,622",
+          "71,622",
+          "71,622",
+          "71,622",
+          "71,622",
+          "71,622"
+        ]
+      },
+      songs: {
+        title: "New releases for you",
+        url: [
+          require("../assets/Images/Image 418.png"),
+          require("../assets/Images/Image 419.png"),
+          require("../assets/Images/Image 693.png"),
+          require("../assets/Images/Image 695.png"),
+          require("../assets/Images/Image 696.png"),
+          require("../assets/Images/Mask Group 237.png")
+        ],
+        name:[
+          'LPS',
+          'Numb Numb Juice',
+          'Drip or Down 2',
+          'Shadows',
+          "Don't Feed the Pop Monster",
+          'HEF'
+        ],
+        detail: [
+          'aaaa',
+          'aaaa',
+          'aaaa',
+          'aaaa',
+          'aaaa',
+          'aaaa'
+        ]
+      },
+      
       artisturl: [
         require("../assets/Images/Mask Group 305.png"),
         require("../assets/Images/Mask Group 321.png"),
@@ -161,9 +186,9 @@ export default {
         require("../assets/Images/Kurt.png"),
         require("../assets/Images/Bowie.png")
       ],
-      productive:"Make monday more producitve",
-      playlists: "Playlists picks",
-      realeased: "New releases for you",
+      // productive:"Make monday more producitve",
+      // playlists: "Playlists picks",
+      // realeased: "New releases for you",
       playlist: true
     }
   }
