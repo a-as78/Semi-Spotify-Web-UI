19a6eae6e347e9c4f5baac4a8378e42d18be4c53
Anahita <anahita.as78@gmail.com>
2020-11-16 12:21:44 +0330

    sign up page needs changes in header

---
+++ src/views/signUp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="signup-body">
+        <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.signup-body{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #161A1A;
+}
+
+</style>
